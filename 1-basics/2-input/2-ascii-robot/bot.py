7abd6cafbbb0529dbfa51bbf0403a9e4f0db86ac
Ilettc <4iletc38@solent.ac.uk>
2018-09-27 12:06:31 +0100

    Create bot.py

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,9 @@
+eye = input("Please enter a character for the eye")
+
+
+print("###############")
+print("#             #")
+print("#      %s %s    #"%(eye, eye))
+print("   |    V   |")
+print("   |   ---- |")
+print("   |________|")
