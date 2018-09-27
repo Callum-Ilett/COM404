7fbce71936b6bf7e2d8eb8539d31f1b1811350d5
Ilettc <4iletc38@solent.ac.uk>
2018-09-27 12:44:20 +0100

    Create bot.py

---
+++ 1-basics/2-input/4-string-operators/bot.py
@@ -0,0 +1,8 @@
+lives = int(input("Please enter the number of lives remaining:"))
+energy = int(input("Please enter the energy level:"))
+lives = int(input("Please enter the shield level"))
+
+print("Lives:")
+print("Energy" )
+
+# I couldnt find the characters
