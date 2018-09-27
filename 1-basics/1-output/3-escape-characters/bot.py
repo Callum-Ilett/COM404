81b70f536764c5e701ff82b8f3282b0c39e94039
Ilettc <4iletc38@solent.ac.uk>
2018-09-27 10:46:22 +0100

    Create bot.py
    Used escape characters such as new line, tab and used quotation marks

---
+++ 1-basics/1-output/3-escape-characters/bot.py
@@ -0,0 +1,8 @@
+#Display escape characters
+print("\n Display a new line")
+print("\t Display a tab space")
+print("\\ Display a back slash")
+print("\" Display a double quote ")
+print("\' Display a single quote.")
+
+print("\t \"You miss 100% of the shots you dont take\"")
