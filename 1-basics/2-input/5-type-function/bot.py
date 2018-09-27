fb424a743de0e2ba3a9e4d27b3aa1b898077634a
Ilettc <4iletc38@solent.ac.uk>
2018-09-27 12:50:39 +0100

    Create bot.py

---
+++ 1-basics/2-input/5-type-function/bot.py
@@ -0,0 +1,7 @@
+#Test the type of different variables
+first_value = 30.5
+second_value = 5
+
+print(first_value, "is of type", type(first_value))
+
+print(first_value, "+", second_value, "is of type", type(first_value + second_value))
