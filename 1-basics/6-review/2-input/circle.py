42c05c88a11ba583210f694153481b5ce51565a7
Ilettc <4iletc38@solent.ac.uk>
2018-10-04 09:31:36 +0100

    Create circle.py
    Calculate area & circumference of a circle

---
+++ 1-basics/6-review/2-input/circle.py
@@ -0,0 +1,8 @@
+import math
+
+radius = float(input("Please enter a radius"))
+area = math.pi * radius**2
+circumfrence = 2 * math.pi * radius
+
+print("\nArea is", area)
+print("Circumfrence is", circumfrence)
