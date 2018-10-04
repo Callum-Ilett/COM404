71a65738f8e86480e55356ac5c99d226a5fdf30d
Ilettc <4iletc38@solent.ac.uk>
2018-10-04 10:29:36 +0100

    Create bot.py

---
+++ 1-basics/3-decisions/2-if-else/bot.py
@@ -0,0 +1,7 @@
+direction = input("Please enter a direction I should move in using WASD:").upper()
+
+if (direction == "W"):
+    print("Moving forward")
+  
+else:
+    print("Sorry I don't know what direction that is, try using WASD")
