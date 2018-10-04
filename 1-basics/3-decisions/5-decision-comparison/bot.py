d40b0211185114d4add505e31de6a69c35eef7e2
Ilettc <4iletc38@solent.ac.uk>
2018-10-04 10:52:38 +0100

    Create bot.py

---
+++ 1-basics/3-decisions/5-decision-comparison/bot.py
@@ -0,0 +1,9 @@
+num1 = int(input("Please enter a number"))
+num2 = int(input("Please enter another number"))
+
+if(num1>num2):
+    print("The first number is bigger")
+elif(num1<num2):
+    print("The second number is bigger")
+else:
+    print("They are both equal")
