6721268fbb7415bd5a289bad724dad283d83bb35
Ilettc <4iletc38@solent.ac.uk>
2018-10-04 09:43:57 +0100

    Create 2-age.py

---
+++ 1-basics/6-review/2-input/2-age.py
@@ -0,0 +1,7 @@
+#Declare useful consatnts
+DAYS_IN_YEAR = 365.25
+
+age_years = int(input("What is your age in years: "))
+age_days = age_years * DAYS_IN_YEAR
+
+print("Your age in days is", age_days)
