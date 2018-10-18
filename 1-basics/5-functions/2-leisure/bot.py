c3432678d71235ee0acf9d17fee4da9b83f4d1e4
Ilettc <4iletc38@solent.ac.uk>
2018-10-18 14:38:50 +0100

    Create bot.py

---
+++ 1-basics/5-functions/2-leisure/bot.py
@@ -0,0 +1,10 @@
+def leisure_activity():
+  activity = input("Name an activity")
+  if(activity == "watch movie"):
+    print("watching a movie sounds like fun")
+  elif(activity == "play"):
+    print("I have lots of toys to play with.")
+  else:
+    print("I am not sure if I will like it but I will give it a try.")
+
+leisure_activity()
