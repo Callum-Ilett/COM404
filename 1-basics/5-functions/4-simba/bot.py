46125f1b99c34d1bcacc20ab7287f3e5c749d94d
Ilettc <4iletc38@solent.ac.uk>
2018-10-18 14:47:09 +0100

    Create bot.py

---
+++ 1-basics/5-functions/4-simba/bot.py
@@ -0,0 +1,15 @@
+def roar(num_roars):
+	# Write your code here (remember to indent correctly)
+  print("Simba says:")
+  for count in range(num_roars):
+    print("roar!")
+    if(num_roars < 3):
+      print("cough! cough!")
+  print("ROAR!!!")
+    
+
+  
+# The following are calls to the function for testing purposes
+roar(1)
+print("")
+roar(3)
