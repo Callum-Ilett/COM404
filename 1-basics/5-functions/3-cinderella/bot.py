02acf72bc9bac9f05038fc4b1d6de44b137f5d82
Ilettc <4iletc38@solent.ac.uk>
2018-10-18 14:41:50 +0100

    Create bot.py

---
+++ 1-basics/5-functions/3-cinderella/bot.py
@@ -0,0 +1,14 @@
+def try_glass_slipper_on(person):
+  if(person == "Eldest step sister"):
+    print( "You are not my princess. Your feet are too big!")
+  elif(person == "Youngest step sister"):
+    print("You are not my princess. Your feet are too small!" )
+
+  else:
+    print("I found my princess!")
+	# Write your code here (remember to indent correctly)
+  
+# The following are calls to the function for testing purposes
+try_glass_slipper_on("Eldest step sister")
+try_glass_slipper_on("Youngest step sister")
+try_glass_slipper_on("Cinderella")
