d5e0ee7ecb493dcb19761df10a822bf3ec5db695
Ilettc <4iletc38@solent.ac.uk>
2018-11-01 10:19:37 +0000

    Create bot.py

---
+++ 2-guis/1-classes-and-objects/bot.py
@@ -0,0 +1,70 @@
+class Bot:
+
+  def __init__(self, name, age=0, energy=0, shield_level=0):
+    self.name = name
+    self.age = age
+    self.energy = energy
+    self.shield_level = shield_level
+
+  def get_name(self):
+    bot_name = self.name
+
+  def get_age(self):
+    bot_name = self.age
+
+  def get_energy(self):
+    bot_energy = self.energy
+
+  def get_shield_level(self):
+    bot_shield = self.shield_level
+
+  def decrement_energy(self):
+    self.energy -= 0
+
+  def decrement_shield(self):
+    self.shield_level -= 0
+    
+  def display_name(self):
+    print(self.name)
+
+  def display_age(self):
+    print(self.age)
+
+  def display_energy(self):
+    print(self.energy)
+
+  def display_shield(self):
+    print(self.shield_level)
+
+  def increment_age(self):
+    self.age += 0
+
+  def increment_energy(self):
+    self.energy += 0
+
+  def increment_shield(self):
+    self.shield_level += 0
+
+#-----------------------------------------------
+class SuperBot(Bot):
+
+  def __init__(self, super_power_level):
+    self.super_power_level = super_power_level
+
+  def get_super_power_level(self):
+    return(self.super_power_level)
+
+  def set_super_power_level(self):
+    self.super_power_level = super_power_level
+
+# ----------------------------------------------
+class FlyingBot(SuperBot):
+
+  def __init__(self, hover):
+    self.hover = hover
+
+  def get_hover_distance(self):
+    hover_distance = self.hover
+
+  def set_hover_distance(self):
+    self.hover = hover
